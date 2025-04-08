7f20df16a7e4dc1484b8846a2c0c9d5907149b32
David Velasquez <velasquezaguilerad@gmail.com>
2025-04-07 23:31:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+//Hamburger//
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+
+hamburgerEl.addEventListener('click', () => { 
+    navEl.classList.toggle('nav--open');
+    hamburgerEl.classList.toggle('hamburger--open');
+});
+
+//slideshow
+
+
 //slideshow
 
 const slides = document.querySelectorAll(".slides img");
