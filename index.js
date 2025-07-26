66c015ce90bed6014159b0452fecfa3dbe54e062
David Velasquez <velasquezaguilerad@gmail.com>
2025-07-25 22:08:17 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-
-//Hamburger//
 const navEl = document.querySelector('.nav');
-const hamburgerEl = document.querySelector('.hamburger');
+const hamburgerEL = document.querySelector('.hamburger');
 
-hamburgerEl.addEventListener('click', () => { 
-    navEl.classList.toggle('nav--open');
-    hamburgerEl.classList.toggle('hamburger--open');
+hamburgerEL.addEventListener('click', () => {
+navEl.classList.toggle('nav--open');
+hamburgerEL.classList.toggle('hamburger--open');
+
 });
 
-//slideshow
-window.onload = function () {
-  const timeSection = document.getElementById("time-section");
-  const dropdown = document.getElementById("hours-dropdown");
-
-  if (timeSection && dropdown) {
-    timeSection.onclick = function (event) {
-      event.stopPropagation();
-      dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-    };
-
-    document.addEventListener("click", function () {
-      dropdown.style.display = "none";
-    });
-  }
-};
